1a41dab0d449191a21dda8dcc6ad7c4a991c0240
ajitk <akadari@gmail.com>
2018-04-16 21:10:40 -0500

    First commit

---
+++ src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
@@ -0,0 +1,32 @@
+package io.pivotal.cloudnativespring;
+
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
+public class CloudNativeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeSpringApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+
+	@Bean
+	JvmThreadMetrics threadMetrics(){
+		return new JvmThreadMetrics();
+	}
+}
